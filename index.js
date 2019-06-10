03e212d94395e3e0328c59a014737106f864fcc8
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-10 07:46:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!';
   } else if (feet > 2000) {
-    return 'I will gladly take your thirty bucks.'
+    
   } else if (feet > 2500) {
     return 'No can do.'
+    return 'I will gladly take your thirty bucks.'
   }
 }
