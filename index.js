10cdbdeeece6ea39c7bfe6f423291aacc6a45106
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-10 07:40:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
-   return 'This one is on me!';
+    return 'This one is on me!';
+  } else if (feet > 2000) {
+      return
   }
 }
